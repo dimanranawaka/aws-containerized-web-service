5af61ec8bd52d122d07c6774fb56021b08add49f
dimanranawaka <dimanranawaka@gmail.com>
2024-09-11 03:53:38 +0530

    Test_01

--- crypto-backend/index.js
+++ crypto-backend/index.js
@@ -46,3 +46,5 @@ app.get('*', (req, res) => {
 // Set the port for the application
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Test_01
